401cfd751aa04039420b5f259b474e5d6e95472f
K0co <chloe.lehoux@free.fr>
2019-04-16 14:09:13 +0200

    Update message5.py

--- message5.py
+++ message5.py
@@ -5,3 +5,25 @@ Created on Fri Apr 12 09:07:21 2019
 @author: chloe
 """
 
+from math import ceil
+import collections
+
+#encoding='utf-8' permet la lecture du message 
+# /!\ changer l'emplacement du message
+with open ("message5.txt","r",encoding='utf-8') as file:
+    message = file.read()
+    
+    print(message)
+    
+    
+    def decodeVigenere(messageLu, cle):
+        
+        decrypté = ''
+        
+        for i in range(len(cle)):
+            
+            
+            
+            
+    
+    
