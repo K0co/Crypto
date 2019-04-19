394b464339833634e49f959ff8801ac5256add4b
K0co <chloe.lehoux@free.fr>
2019-04-19 09:08:01 +0200

    Create Chloé.py

---
+++ Chloé.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 01:07:48 2019
+
+@author: chloe
+"""
+
+from math import ceil
+import collections
+
+#encoding='utf-8' permet la lecture du message 
+# /!\ changer l'emplacement du message
+with open ("chloe.txt","r",encoding='latin') as file:
+    message = file.read()
+    
+    print(message)
+
+
+        
