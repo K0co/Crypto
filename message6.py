2743ca8854b5ce05cc81640fede5718b4abf99ac
K0co <chloe.lehoux@free.fr>
2019-04-19 09:44:10 +0200

    Update message6.py

--- message6.py
+++ message6.py
@@ -56,7 +56,7 @@ with open ("message6.txt","r",encoding='utf-8') as file:
         return decrypte
         
         
-key = cleVigenere(message, 3)
+key = cleVigenere(message, 11)
 texte = decodeVigenere(message, key)
 
 print(key,'\n', texte)
