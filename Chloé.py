3c45f7ced8c33b072f5608c4e6eecafe08139ebc
K0co <chloe.lehoux@free.fr>
2019-04-19 09:27:50 +0200

    Update Chloé.py

--- Chloé.py
+++ Chloé.py
@@ -15,5 +15,52 @@ with open ("chloe.txt","r",encoding='latin') as file:
     
     print(message)
 
+    
+    def cleVigenere(messageLu, longueur):
+        '''Trouve la Cle pour n'importe quel message
+        '''
+    
+        cle = [0] * longueur
+        chaine = [''] *longueur
+        i = 0
+        
+        for charac in message :
+            
+            chaine[i%longueur] = chaine[i%longueur] + charac
+
+            i += 1
+          
+        for i in range(longueur) :
+            
+            freqCommon = collections.Counter(chaine[i]).most_common(1)
+
+            cle[i] = ord(freqCommon[0][0]) - ord(' ')
+            
+        return cle
+        
+        
+    def decodeVigenere(messageLu, cle):
+        '''Récupère la cle pour déchiffrer le message
+        '''
+        
+        longueur = len(cle)
+        decrypte = ""
+        i = 0
+        
+        for charac in messageLu :
+            #on soustrait l'emplacement du charactère par la cle pour décoder
+            decal = ord(charac) - cle[i%longueur]
+            #on ajoute le bon charactère dans le chaine de charactère
+            decrypte = decrypte + chr(decal)
+            
+            i += 1
+            
+        return decrypte
+        
+        
+key = cleVigenere(message, 3)
+texte = decodeVigenere(message, key)
+
+print(key,'\n', texte)
 
         
